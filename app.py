8786ad30ca0f6702fcb3e232cc2e664fea3bcb8a
prekshivyas <prekshivyas@gmail.com>
2024-07-11 00:31:23 +0530

    changes push

--- app.py
+++ app.py
@@ -320,7 +320,7 @@ if choose == "About Me":
             index = st.session_state[key]
 
             # Display the current image
-            with st.container(height=350):
+            with st.container(height=355):
                 st.image(images[index], use_column_width=True)
 
 
